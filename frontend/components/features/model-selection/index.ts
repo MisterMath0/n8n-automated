372422ac04b96a04867f7ac50f0b38dba567197e
Guy Mathieu FOKO <guymathieuf@gmail.com>
2025-06-20 00:47:24 +0200

    refactor(model-selection): remove ModelSelectionGrid and ModelSelector components to streamline model selection process

--- frontend/components/features/model-selection/index.ts
+++
@@ -1,4 +0,0 @@
-// Export model selection components
-export { ModelSelectionGrid } from './ModelSelectionGrid';
-export { ModelSelector } from './ModelSelector';
-export { ModelCard } from './ModelSelectionGrid';
